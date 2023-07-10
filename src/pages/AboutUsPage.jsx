73f1fe84001e996755656e07122631b94c5e382b
Avi Konduru <avikonduru@gmail.com>
2023-07-10 04:43:30 -0700

    rest

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -2,11 +2,20 @@ import { Fragment } from 'react';
 import { Layout } from '../layout/Layout';
 
 // components
+import { TeamSection } from '../component/AboutUsComponents/TeamSection';
+import { KeyDifferentiatorsSection } from '../component/AboutUsComponents/KeyDifferentiatorsSection';
+import { CompanyHistorySection } from '../component/AboutUsComponents/CompanyHistorySection';
+import { MissionAndValuesSection } from '../component/AboutUsComponents/MissionAndValuesSection';
 
 const AboutUsPage = () => {
   return (
     <Fragment>
-      <Layout>About Us Page</Layout>
+      <Layout>
+        <CompanyHistorySection />
+        <MissionAndValuesSection />
+        <TeamSection />
+        <KeyDifferentiatorsSection />
+      </Layout>
     </Fragment>
   );
 };
