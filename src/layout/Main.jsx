220bea41df442cd4f41542c6985cb110e80b0c71
Avi Konduru <avikonduru@gmail.com>
2024-07-07 09:51:42 -0700

    init nextjs

--- src/layout/Main.jsx
+++
@@ -1,9 +0,0 @@
-import { Container, Flex } from '@chakra-ui/react';
-
-export const Main = props => {
-  return (
-    <Flex as="main" role="main" direction="column" flex="1" {...props}>
-      <Container flex="1">{props.children}</Container>
-    </Flex>
-  );
-};
