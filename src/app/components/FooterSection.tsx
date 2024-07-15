6912251908bc204fc549a1b94fe8404206b5ab26
Avi Konduru <avikonduru@gmail.com>
2024-07-15 14:21:47 -0700

    init footer

---
+++ src/app/components/FooterSection.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// radix
+import {
+	Container,
+	Box,
+	Flex,
+	Button,
+	Text,
+	Link,
+	Grid,
+	Section,
+} from '@radix-ui/themes';
+
+const FooterSection = () => {
+	return (
+		<Box
+			style={{
+				background: '#FFFFFF',
+				padding: '50px 0px',
+				color: 'black',
+			}}>
+			Footer section
+		</Box>
+	);
+};
+
+export default FooterSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import Image from 'next/image';
 
 // components
-// import Navbar from './components/old/Navbar';
-// import HeroSection from './components/old/HeroSection';
-// import ServicesSection from './components/old/ServicesSection';
-// import AboutSection from './components/old/AboutSection';
-import FooterSection from './components/old/FooterSection';
-
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import ServicesSection from './components/ServicesSection';
 import AboutSection from './components/AboutSection';
+import FooterSection from './components/FooterSection';
 
 // radix
 import { Box } from '@radix-ui/themes';
@@ -29,6 +24,7 @@ const HomePage = () => {
 			<HeroSection />
 			<ServicesSection />
 			<AboutSection />
+			<FooterSection />
 		</Box>
 	);
 };
